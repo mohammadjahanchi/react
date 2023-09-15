265af97767caadb360d1f7205d6c79acce404a79
mohammad <MOHAMMADJAHANCHI@GMAIL.COM>
2023-09-15 13:12:10 +0430

    class-components

---
+++ jahanchitest/src/users/User.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+
+// export default class User extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>mohammad1</h1>
+//       </div>
+//     )
+//   }
+// }
+
+const User=()=>{
+    return(
+        <div>
+            <h3>mohammad3</h3>
+        </div>
+    )
+}
+export default User;
