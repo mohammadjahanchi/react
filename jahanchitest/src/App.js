7f0723d3fae53791abce0f3cf9ff00db922babe3
mohammad <MOHAMMADJAHANCHI@GMAIL.COM>
2023-09-16 16:54:42 +0430

    react.ir 4 jsx

--- jahanchitest/src/App.js
+++ jahanchitest/src/App.js
@@ -1,14 +1,11 @@
-import React,{Component} from "react";
-import User from "./users/User";
+import React from "react";
 import './App.css';
-import Product from "./products/Product";
+
 
 const App=()=>{
   return(
     <div className="container">
-      <Product title="کفش اسپرت" image="images/1.jpg" price={120} />
-      <Product title="کفش مجلسی" image="images/4.jpg" price={200} />
-      <Product title="کفش رنگی رنگی" image="images/2.jpg" price={150} />
+      
     </div>
   )
 }
