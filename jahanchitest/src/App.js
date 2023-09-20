1d068bd0c0ade4d9a0a22e60f3d9f2617ed42a8f
mohammad <mohammadjahanchi@gmail.com>
2023-09-20 19:35:21 +0330

    state

--- jahanchitest/src/App.js
+++ jahanchitest/src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import './App.css';
-import { Website } from "./componenet/welcom";
-import Welcom from "./componenet/welcom";
 const App=()=>{
-  const ClickHandler=(name)=>{
-    alert('hi'+''+name)
-  }
-
   return(
-    <div className="App" >
-      <Website name="mohammad" age={27} handler={()=>ClickHandler("Mohammad")} />
-      <Website name="ali" age={24} handler={()=>ClickHandler("Ali")} />
+    <div>
+    <h3>app test</h3>
     </div>
   )
 }
+
 export default App;
--- jahanchitest/src/index.js
+++ jahanchitest/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import State from './componenet/state';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +11,8 @@ root.render(
   
   <React.StrictMode>
     <App />
+    <h1> salam </h1>
+    <State></State>
    
   </React.StrictMode>
 );
